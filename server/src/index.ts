94cca590693f5d73787e8b2d9b9d88876e291aaf
monsefCode <monsefetta4@gmail.com>
2023-04-12 10:53:36 +0000

    api to prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://co-quiz.netlify.app"],
   optionsSuccessStatus: 200,
 };
 
